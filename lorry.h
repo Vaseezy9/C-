277d5dedea326a81c6f82ccea9f30924664c4438
Vaseezy9 <139249278+Vaseezy9@users.noreply.github.com>
2025-06-28 15:48:34 +0300

    Update lorry.h

--- lorry.h
+++ lorry.h
@@ -1,12 +1,17 @@
+#ifndef LORRY_H
+#define LORRY_H
+
 class Lorry : public Car {
 private:
     int load_capacity;
 
 public:
     Lorry(const std::string& brand, int cylinders, int pow, int load capacity) : Car(brand, cylinders, pow), load_capacity(load capacity) {}
-    virtual ~Lorry() {};
+    virtual ~Lorry() {}
 
     friend int count();
     friend std::istream& operator>>(std::istream& is, Lorry& lorry);
     friend std::ostream& operator<<(std::ostream& os, const Lorry& lorry);
 };
+
+#endif
