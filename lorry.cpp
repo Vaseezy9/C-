36e473424af2dcf0910fd015a53f368967531bed
Vaseezy9 <139249278+Vaseezy9@users.noreply.github.com>
2025-06-30 20:59:37 +0300

    Update lorry.cpp

--- lorry.cpp
+++ lorry.cpp
@@ -1,14 +1,15 @@
 #include "lorry.h"
 
 std::istream& operator>>(std::istream& is, Lorry& lorry) {
-    is >> lorry.brand_mark >> lorry.cylinders >> lorry.power >> lorry.load_capacity;
+    is >> lorry.getBrandMark() >> lorry.getCylinders() >> lorry.getPower() >> lorry.load_capacity;
     Car::count_objects++;
     return is;
 }
+
 std::ostream& operator<<(std::ostream& os, const Lorry& lorry) {
-    os << "brand: " << lorry.brand_mark << std::endl
-       << "cylinders: " << lorry.cylinders << std::endl
-       << "power: " << lorry.power << std::endl
+    os << "brand: " << lorry.getBrandMark() << std::endl
+       << "cylinders: " << lorry.getCylinders() << std::endl
+       << "power: " << lorry.getPower() << std::endl
        << "load capacity: " << lorry.load_capacity << std::endl;
     return os;
 }
