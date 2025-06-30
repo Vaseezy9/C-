2313366590799aa272523350f23de1b13b2613a6
Vaseezy9 <139249278+Vaseezy9@users.noreply.github.com>
2025-06-30 21:30:43 +0300

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -12,3 +12,26 @@ std::ostream& operator<<(std::ostream& os, const Car& car) {
        << "power: " << car.power << std::endl;
     return os;
 }
+
+Car::Car() : brand_mark(), cylinders(0), power(0) {}
+Car::Car(const std::string& brand, int cylinders, int pow) : brand_mark(brand), cylinders(cylinders), power(pow) {}
+Car::Car(const Car& other) : brand_mark(other.brand_mark), cylinders(other.cylinders), power(other.power) {}
+Car::~Car() {}
+
+std::string Car::getBrandMark() const { return brand_mark; }
+void Car::setBrandMark(const std::string& brand) { brand_mark = brand; }
+int Car::getCylinders() const { return cylinders; }
+void Car::setCylinders(int cyl) { cylinders = cyl; }
+int Car::getPower() const { return power; }
+void Car::setPower(int pow) { power = pow; }
+
+Car& Car::operator=(const Car& other) {
+    if (this != &other) {
+        brand_mark = other.brand_mark;
+        cylinders = other.cylinders;
+        power = other.power;
+    }
+    return *this;
+}
+
+int Car::count_objects = 0;
