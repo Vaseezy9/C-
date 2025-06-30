4c565ee8b000e349069cfbebba952b6376e99a87
Vaseezy9 <139249278+Vaseezy9@users.noreply.github.com>
2025-06-30 21:33:52 +0300

    Update lorry.cpp

--- lorry.cpp
+++ lorry.cpp
@@ -13,3 +13,25 @@ std::ostream& operator<<(std::ostream& os, const Lorry& lorry) {
        << "load capacity: " << lorry.load_capacity << std::endl;
     return os;
 }
+
+Lorry::Lorry() : Car(), load_capacity(0) {}
+Lorry::Lorry(const std::string& brand, int cylinders, int pow, int load_capacity) : Car(brand, cylinders, pow), load_capacity(load_capacity) {}
+Lorry::Lorry(const Lorry& other) : Car(other), load_capacity(other.load_capacity) {}
+Lorry::~Lorry() {}
+
+std::string Lorry::getBrandMark() const { return static_cast<const Car&>(*this).getBrandMark(); }
+void Lorry::setBrandMark(const std::string& brand) { static_cast<Car&>(*this).setBrandMark(brand); }
+int Lorry::getCylinders() const { return static_cast<const Car&>(*this).getCylinders(); }
+void Lorry::setCylinders(int cyl) { static_cast<Car&>(*this).setCylinders(cyl); }
+int Lorry::getPower() const { return static_cast<const Car&>(*this).getPower(); }
+void Lorry::setPower(int pow) { static_cast<Car&>(*this).setPower(pow); }
+int Lorry::getLoadCapacity() const { return load_capacity; }
+void Lorry::setLoadCapacity(int load) { load_capacity = load; }
+
+Lorry& Lorry::operator=(const Lorry& other) {
+    if (this != &other) {
+        *this = static_cast<const Car&>(other);
+        load_capacity = other.load_capacity;
+    }
+    return *this;
+}
